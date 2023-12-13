f41b3cc9f1b1de99c9650c2eedda0529cf4a6640
Neil Burningham <neil.burningham@mediakind.com>
2023-12-13 16:22:05 +0000

    Switch to Player cmpt, edit scaling mode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
 
 fn main() {
     App::new()
@@ -13,7 +14,7 @@ fn main() {
                     }),
                     ..default()
                 })
-                .build()
+                .build(),
         )
         .add_systems(Startup, setup)
         .add_systems(Update, character_movement)
@@ -21,37 +22,53 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
+    let mut camera = Camera2dBundle::default();
+
+    camera.projection.scaling_mode = ScalingMode::AutoMin {
+        min_width: 256.0,
+        min_height: 144.0,
+    };
+    commands.spawn(camera);
 
     let texture = asset_server.load("character.png");
 
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(100.0, 100.0)),
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(16.0, 16.0)), // TODO: use 16x16 sprite & remove this!
+                ..default()
+            },
+            texture,
             ..default()
         },
-        texture,
-        ..default()
-    });
+        Player { speed: 100.0 },
+    ));
 }
 
 fn character_movement(
-    mut characters: Query<&mut Transform, With<Sprite>>,
+    mut characters: Query<(&mut Transform, &Player)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    for mut transform in &mut characters {
+    for (mut transform, player) in &mut characters {
+        let movement_distance = player.speed * time.delta_seconds();
+
         if input.pressed(KeyCode::W) {
-            transform.translation.y += 100.0 * time.delta_seconds();
+            transform.translation.y += movement_distance;
         }
         if input.pressed(KeyCode::S) {
-            transform.translation.y -= 100.0 * time.delta_seconds();
+            transform.translation.y -= movement_distance;
         }
         if input.pressed(KeyCode::A) {
-            transform.translation.x -= 100.0 * time.delta_seconds();
+            transform.translation.x -= movement_distance;
         }
         if input.pressed(KeyCode::D) {
-            transform.translation.x += 100.0 * time.delta_seconds();
+            transform.translation.x += movement_distance;
         }
     }
+}
+
+#[derive(Component)]
+pub struct Player {
+    pub speed: f32,
 }
