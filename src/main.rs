bfc6868fdf8611d21fb59922b33b32d069ddfc08
Neil Burningham <neil.burningham@mediakind.com>
2023-12-13 15:14:31 +0000

    Add character movement, switch to default_nearest image plugin

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,21 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "kneilb Rougelike".into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build()
+        )
         .add_systems(Startup, setup)
+        .add_systems(Update, character_movement)
         .run();
 }
 
@@ -20,4 +33,25 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         texture,
         ..default()
     });
+}
+
+fn character_movement(
+    mut characters: Query<(&mut Transform, &Sprite)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, _) in &mut characters {
+        if input.pressed(KeyCode::W) {
+            transform.translation.y += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::S) {
+            transform.translation.y -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A) {
+            transform.translation.x -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += 100.0 * time.delta_seconds();
+        }
+    }
 }
