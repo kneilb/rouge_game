9e9bfff51811800160b5a440114ae1376ba53f64
Neil Burningham <neil.burningham@mediakind.com>
2023-12-19 17:49:10 +0000

    Constants, fix stupid bug

--- src/pig.rs
+++ src/pig.rs
@@ -3,6 +3,9 @@ use bevy::prelude::*;
 
 pub struct PigPlugin;
 
+const PIG_BUY_PRICE: f32 = 10.0;
+const PIG_SELL_PRICE: f32 = 15.0;
+
 impl Plugin for PigPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, (spawn_pig, pig_lifetime))
@@ -29,23 +32,18 @@ fn spawn_pig(
 
     let player_transform = query.single();
 
-    if money.0 < 10.0 {
+    if money.0 < PIG_BUY_PRICE {
         info!("Not enough cash!");
         return;
     }
 
-    money.0 -= 10.0;
-    info!("Spent £10 on a pig, remaining money £{:?}", money.0);
+    money.0 -= PIG_BUY_PRICE;
+    info!("Spent £{} on a pig, remaining money £{:?}", PIG_BUY_PRICE, money.0);
 
     let texture = asset_server.load("pig.png");
 
     commands.spawn((
         SpriteBundle {
-            // TODO: use 16x16 sprite & remove this!
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(16.0, 16.0)),
-                ..default()
-            },
             texture,
             transform: *player_transform,
             ..default()
@@ -67,10 +65,10 @@ fn pig_lifetime(
         pig.lifetime.tick(time.delta());
 
         if pig.lifetime.finished() {
-            money.0 += 15.0;
-            info!("Earnt £15; money is now £{:?}", money.0);
-        }
+            money.0 += PIG_SELL_PRICE;
+            info!("Earnt £{}; money is now £{:?}", PIG_SELL_PRICE, money.0);
 
-        commands.entity(pig_entity).despawn();
+            commands.entity(pig_entity).despawn();
+        }
     }
 }
